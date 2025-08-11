c43142086af3621bf3b4cd00d237f9d2053b04e7
郑浩龙 <zhenghaolong@zhenghaolongdeMac-mini.local>
2025-08-12 01:40:47 +0800

    router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -85,4 +85,13 @@ export const router = createBrowserRouter([
     path: '*',
     element: <NotFound />,
   },
-]);
+], {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
